21db9040dd85991fa0e7e4a041b15cf9fa5bcac9
Dreamcoder2k <rachhepsi20@gmail.com>
2026-05-31 13:39:42 +0530

    strings integers float boolean

---
+++ strings & integers.py
@@ -0,0 +1,38 @@
+#variable = A container for a value (string, integer, float, boolean)
+
+
+#strings
+from operator import truediv
+
+first_name="bro"
+food= "pizza"
+email = "bro@fake.com"
+
+#integers
+age=25
+quantity=3.5
+
+#float
+price =10.99
+gpa = 3.2
+print(f"the price is ${price}")
+print(f"the gpa is: {gpa}")
+
+#boolean
+is_student= False
+for_sale = False
+
+if is_student:
+    print("That item is for sale")
+else:
+    print("that item is not available")
+
+#boolean
+is_student= False
+for_sale = False
+is_online = True
+
+if is_online:
+    print("You are online")
+else:
+    print("you are offline")
