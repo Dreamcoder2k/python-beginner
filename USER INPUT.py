8997c2fed4fb8f04c821bf66df6829886fd7a93c
Dreamcoder2k <rachhepsi20@gmail.com>
2026-05-31 19:00:15 +0530

    calculate area of rectangle

---
+++ USER INPUT.py
@@ -0,0 +1,14 @@
+#input()  receving data from user
+
+name = input("What is your name?:")
+age= input("how old are you")
+age =int(age)
+age = age +1
+
+print(f"Hello {name}!")
+print(f"my age is {age}")
+
+length =int( input("Enter the length:"))
+width = int(input("Enter the width:"))
+area = length * width
+print(f"area= {area}")
