e631a1f31098693a9c2c82ecf7342c77f7838673
Dreamcoder2k <rachhepsi20@gmail.com>
2026-06-14 13:46:28 +0530

    conditional or ternary operator

---
+++ Conditional Operator.py
@@ -0,0 +1,11 @@
+#Conditional or Ternary Operator
+# print or assign one of two values based on a condition
+# X if condition else y
+
+
+num = 6
+age = 8
+x= "WEEKEND!" if num > 5 else "WORKDAY"
+status = "Adult" if age >= 18 else "child"
+print(x)
+print(status)
