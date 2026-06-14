dd7e0c077f433c36ad2146d1f833bd6803d4f869
Dreamcoder2k <rachhepsi20@gmail.com>
2026-06-14 19:47:03 +0530

    string index

--- String Index.py
+++ String Index.py
@@ -1,5 +1,11 @@
 # String index accessing elements of sequence using []
+
+phone_number = "1234-5678-9870"
+#print(phone_number [0])
+#print(phone_number [5:9])
+#print(phone_number [4:])
+
 credit_number = "1233-8577-9897"
 
-last_digits = credit_number[-4:]
+last_digits = credit_number[-4:1]
 print(f"xxxx-xxxx-xxxx-{last_digits}")
