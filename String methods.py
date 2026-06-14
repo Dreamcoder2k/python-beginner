4056fa4af11b2fe06e9d23295986fb71d74539de
Dreamcoder2k <rachhepsi20@gmail.com>
2026-06-14 14:44:26 +0530

    String methods

---
+++ String methods.py
@@ -0,0 +1,18 @@
+#name = input("Enter your full name")
+
+phone_number = input("Enter your phone number:")
+
+#result = len(name)
+#result = name.find("K")
+#result = name.rfind("q")
+#name= name.capitalize()
+#name = name.upper()
+#name = name.lower()
+#name = name.isdigit()
+#name = name.isalpha()
+#name = name.isdecimal()
+#name= phone_number.count("-")
+#name= phone_number.replace("-","")
+
+
+print(phone_number)
