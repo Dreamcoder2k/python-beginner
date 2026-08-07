d9605118ad6a4b422a10d0a74fe4f163e48fec0a
Dreamcoder2k <rachhepsi20@gmail.com>
2026-08-07 07:39:57 +0530

    Intro for Dictionaries

---
+++ Dictionaries.py
@@ -0,0 +1,28 @@
+#dictionaries
+
+places={ "China": "Wuxi",
+         "Chennai": "Anna Tower park",
+         "Thanjavur": "Periya kovil",
+         }
+
+#print(dir(places))
+#print(help(places))
+#print(places.get("Japan"))
+
+#if places.get("Japan"):
+    #print("That place exists")
+#else:
+   # print("The place doesn't exists")
+
+
+#places.update({"India":"Chennai"})
+#print(places)
+
+#keys = places.keys()
+
+#for key in places.keys():
+   # print(key)
+
+#values= places.values()
+#for value in places.values():
+   # print(value)
