05beb71ebd2f0e345a9d739a0c76c6c00560b018
Dreamcoder2k <rachhepsi20@gmail.com>
2026-08-25 07:51:42 +0530

    Just Functions

---
+++ Functions.py
@@ -0,0 +1,7 @@
+#Functions is known as if I call my code at any time like it is a reusable code
+
+def display_invoice(username, amount, due_date):
+    print(f"Hello {username}")
+    print(f"Your due amount is ${amount}, you have to pay within this {due_date}")
+
+display_invoice("Kanmani",500, "01/08/2026" )
